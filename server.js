0e5b61f5ac86b209c317513b6fc748284fbae403
Zamar443 <165084526+Zamar443@users.noreply.github.com.>
2026-03-22 13:48:09 +0100

    fix case sensitive for User.js

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ socketHandler(io);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    server.listen(5000, () => console.log("Server running on port 5000"));
+    const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch(err => console.error(err));
